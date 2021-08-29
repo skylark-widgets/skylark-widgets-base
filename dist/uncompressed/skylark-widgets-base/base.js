9df2596c123043daa2873bcc45014b8f5f10949e
hudaokeji <github@hudaokeji.com>
2021-08-29 16:31:01 +0900

    rename module file name

---
+++ dist/uncompressed/skylark-widgets-base/base.js
@@ -0,0 +1,12 @@
+define([
+	"skylark-langx-ns"
+],function(skylark){
+	return skylark.attach("widgets.base",{
+		"actions":{},
+		"dnd" : {},
+		"locales" : {},
+		"mixins" : {},
+		"panels" : {},
+		"skins" : {}
+	});
+});
---
+++ dist/uncompressed/skylark-widgets-base/main.js
@@ -0,0 +1,18 @@
+define([
+	"./base",
+	"./widget",
+	"./canvas-pane",
+	"./image-pane",
+	"./text-pane",
+	"./submit-form",
+	"./actions/action",
+	"./actions/action-manager",
+	"./panels/dual-container",
+    "./panels/dual-panel",
+    "./panels/rows-panel",
+    "./dnd/drag-buffer",
+	"./skins/skin-manager",
+	"./skins/skin-dark"
+],function(base){
+	return base;
+});
